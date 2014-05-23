df9031e0cc95083a45c2dc98b76603ac6c37a9b9
shiplem3 <shiplem3@students.wwu.edu>
2014-05-23 13:33:49 -0700

    Create quizzes.php

---
+++ application/controllers/quizzes.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once "BaseController.php";
+/*
+ * This class is the Quizzes controller.
+ * This controller is found at the URI '/quizzes'
+ */
+class Welcome extends BaseController {
+
+	function __construct(){
+		parent::__construct();
+	}
+
+}
+
+/* End of file quizzes.php */
+/* Location: ./application/controllers/quizzes.php */
