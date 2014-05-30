e041cceef3f4e080bd244236e17c13adb0df2603
Rémi GATTAZ <remi.gattaz@free.fr>
2014-05-29 22:48:44 -0700

    Add user session

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once "BaseController.php";
+require_once "baseController.php";
 /*
  * This class is the Welcome controller.
  * This controller is found at the URI '/' and '/welcome'
